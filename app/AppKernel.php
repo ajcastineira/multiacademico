e8231123e7cf422aef08de27d641b2b5e4742d76
renearias <rene_811@hotmail.com>
2015-10-09 21:24:42 -0500

    Multiacademico Bundle para compatibilidad con Database

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new MultiacademicoBundle\MultiacademicoBundle(),
             new Multiservices\ArxisBundle\MultiservicesArxisBundle(),
+            new Multiservices\NotifyBundle\NotifyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

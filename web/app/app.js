5fd988ff5c700cc83dee74e27a5d11b99fbf40ef
Gipsy <Gipsy@Gipsy-PC>
2015-10-20 02:49:15 -0430

    conflictos resueltos app.js

--- web/app/app.js
+++ web/app/app.js
@@ -98,3 +98,105 @@ define([
  
     return app;
 });
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name app [arxisApp]
+ * @description
+ * # app [arxisApp]
+ *
+ * Main module of the application.
+ */
+
+define([
+    'angular',
+    'angular-couch-potato',
+    'angular-ui-router',
+    'angular-animate',
+    'angular-sanitize',
+    'angular-bootstrap',
+    'angular-loading-bar',
+    'oc-lazyload'
+], function (ng, couchPotato) {
+
+    
+    var app = ng.module('app', [
+        'ngSanitize',
+        'ngAnimate',
+        'oc.lazyLoad',
+        'scs.couch-potato',
+        'ui.router',
+        'ui.bootstrap',
+        'angular-loading-bar',
+        'blankonConfig',
+        'blankonController',
+        'blankonDirective',
+        //app
+        'app.auth',
+        'app.dashboard',
+        //'app.users',
+        'app.layout',
+        'app.forms',
+        'multiacademico',
+        'multiacademico.estudiantes',
+        'multiacademico.docentes.midistributivo',
+        'multiacademico.proyectosescolares'
+
+    ]);
+        
+   couchPotato.configureApp(app);
+   
+    app.config(function ($provide, $httpProvider, $locationProvider) {
+
+       $locationProvider.html5Mode(true);
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+        // Intercept http calls.
+        $provide.factory('ErrorHttpInterceptor', function ($q) {
+            var errorCounter = 0;
+            function notifyError(rejection){
+                console.log(rejection);
+               /* $.bigBox({
+                    title: rejection.status + ' ' + rejection.statusText,
+                    content: rejection.data,
+                    color: "#C46A69",
+                    icon: "fa fa-warning shake animated",
+                    number: ++errorCounter,
+                    timeout: 6000
+                });*/
+          }
+
+            return {
+                // On request failure
+                requestError: function (rejection) {
+                    // show notification
+                    notifyError(rejection);
+
+                    // Return the promise rejection.
+                    return $q.reject(rejection);
+                },
+
+                // On response failure
+                responseError: function (rejection) {
+                    // show notification
+                    notifyError(rejection);
+                    // Return the promise rejection.
+                    return $q.reject(rejection);
+                }
+            };
+        });
+        // Add the interceptor to the $httpProvider.
+        $httpProvider.interceptors.push('ErrorHttpInterceptor');
+         
+    });
+
+    app.run(function ($couchPotato, $rootScope, $state, $stateParams) {
+        app.lazy = $couchPotato;
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+        // editableOptions.theme = 'bs3';
+    });
+ 
+    return app;
+});

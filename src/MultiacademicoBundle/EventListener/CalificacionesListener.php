d08971e5cbc5b90dfd5ab1669e500fd508ac0001
Multiservices <Multiservices@user1>
2015-11-03 22:11:34 -0500

    Por fin funciona el escucha de la persistencia de la database

--- src/MultiacademicoBundle/EventListener/CalificacionesListener.php
+++ src/MultiacademicoBundle/EventListener/CalificacionesListener.php
@@ -6,14 +6,11 @@
 
 namespace MultiacademicoBundle\EventListener;
 
-use Doctrine\ORM\Mapping as ORM;
-//use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
-use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
-use Doctrine\ORM\Event\OnFlushEventArgs;
+
 use Doctrine\ORM\Event\PostFlushEventArgs;
 use MultiacademicoBundle\Entity\Calificaciones;
-use Multiservices\NotifyBundle\Servicios\Notificador;
+
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -21,7 +18,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @author Rene Arias
  */
-class CalificacionesListener {
+class CalificacionesListener  {
     
    
     protected $container;
@@ -32,17 +29,9 @@ class CalificacionesListener {
         $this->container = $container;
     }
 
-     
-
-    public function preUpdate(PreUpdateEventArgs $args)
+    public function preUpdate(Calificaciones $calificacion, PreUpdateEventArgs $args)
     {
-        $entity = $args->getEntity();
 
-        // False check is compulsory otherwise duplication occurs
-        if (($entity instanceof Calificaciones) === false) {
-            //$userLog = new UserLog();
-            //$userLog->setDescription($entity->getId() . ' being updated.');
-            $calificacion=$entity;
             $user = $this->container->get('security.token_storage')->getToken()->getUser();
                 $notificador=$this->container->get('notificador');
                 $usuarioanotificar=$calificacion->getCalificacionnummatricula()->getMatriculacodestudiante()->getUsuario();
@@ -50,36 +39,25 @@ class CalificacionesListener {
                 $actiondata=new \MultiacademicoBundle\ActionData\DocenteWtriteCalificacionYou();
                   $actiondata->setDocente($user->getName());
                   $actiondata->setMateria($calificacion->getCalificacioncodmateria()->getMateria());
-                  $n= new \Multiservices\NotifyBundle\Entity\Notificaciones();
-                  $n->setActionid($actiondata->getActionid());
-                  $n->setNotificaciontitulo($user->getName());
-                  $n->setVariables($actiondata);
-                  $n->setNotificacionuser($usuarioanotificar);
-                  //$n=$notificador->crearNotificacion($actiondata->getActionid(), $user->getName(), $usuarioanotificar,$actiondata);
-            
-
-            $this->calificacionesPorNotificar[] = $n;
-        }
+                  
+                  $n=$notificador->crearNotificacion($actiondata->getActionid(),$user->getName(),$usuarioanotificar,$actiondata);
+                  $this->calificacionesPorNotificar[] = $n;
     }
    
     
     public function postFlush(PostFlushEventArgs $args)
     {
+        
         if (!empty($this->calificacionesPorNotificar)) {
             $em = $args->getEntityManager();
             foreach ($this->calificacionesPorNotificar as $notificacion) {
                 
-                
-                
-                 
-                
-                
                 $em->persist($notificacion);
-            
-                
-                
+                 
             }
+            $this->calificacionesPorNotificar=[];
             $em->flush();
-        }
+            
+       }
     }
 }

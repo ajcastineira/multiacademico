79d4ca001f8e9d736b9521839fd5b83fac422c95
renearias <rene_811@hotmail.com>
2016-01-26 08:25:54 +0100

    update con composer

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 echo "Actualizando Multiacademico"
 git reset --hard
 git pull --rebase
+php composer.phar update
 php bin/console cache:clear --env prod
 php bin/console doctrine:schema:update --dump-sql --force
 php bin/console doctrine:fixtures:load --append

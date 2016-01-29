602aaab4c748081035689d6842dd42a0e178bcc5
renearias <rene_811@hotmail.com>
2016-01-29 11:29:44 -0500

    index redirect a 755

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ php composer.phar update
 php bin/console cache:clear --env prod
 php bin/console doctrine:schema:update --dump-sql --force
 php bin/console doctrine:fixtures:load --append
+chmod 755 index.php
 chmod 755 web/app.php
 chmod 755 web/app_dev.php.php

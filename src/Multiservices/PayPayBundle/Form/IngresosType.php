9d21a07951fdb280ebf73051dcfbae2fe0cb317c
renearias <rene_811@hotmail.com>
2016-02-12 16:27:53 -0500

    formulario actualziado

--- src/Multiservices/PayPayBundle/Form/IngresosType.php
+++ src/Multiservices/PayPayBundle/Form/IngresosType.php
@@ -5,10 +5,6 @@ namespace Multiservices\PayPayBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Multiservices\LexBundle\Entity\Contactos;
 
 class IngresosType extends AbstractType
 {
@@ -19,47 +15,15 @@ class IngresosType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-         //   ->add('fecha')
-            ->add('cliente','cliente')
+           // ->add('fecha', 'datetime')
             ->add('monto')
-          //  ->add('caso')
             ->add('descripcion')
             ->add('referencia')
+            ->add('collectedby')
+            ->add('modifiedby')
             ->add('formaPago')
+            ->add('representante')
         ;
-        
-        $formModifier = function (FormInterface $form, Contactos $cliente = null) {
-             $casos = null === $cliente ? array() : $cliente->getCasos();
-            $form->add('caso', 'entity', array(
-                    'class'       => 'LexBundle:Casos',
-                    'placeholder' => '',
-                    'choices'     => $casos,
-                    'required' => false
-                ));
-        };
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formModifier) {
-                // this would be your entity, i.e. SportMeetup
-                $data = $event->getData();
-
-                $formModifier($event->getForm(), $data->getCliente());
-            }
-        );
-
-        $builder->get('cliente')->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($formModifier) {
-                // It's important here to fetch $event->getForm()->getData(), as
-                // $event->getData() will get you the client data (that is, the ID)
-                $sport = $event->getForm()->getData();
-                // since we've added the listener to the child, we'll have to pass on
-                // the parent to the callback functions!
-                $formModifier($event->getForm()->getParent(), $sport);
-            }
-        );
-
     }
     
     /**
@@ -69,15 +33,7 @@ class IngresosType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => 'Multiservices\PayPayBundle\Entity\Ingresos',
-            'attr' => array('ng-submit'=>"processForm('".$this->getName()."')")
+            'attr' => array('ng-submit'=>"processForm(\$event,'ingresos')")
         ));
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'multiservices_paypaybundle_ingresos';
     }
 }

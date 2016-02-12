a41afb6941a28677ee72d06f3356f30680053096
renearias <rene_811@hotmail.com>
2016-02-12 16:24:21 -0500

    data fixtures en formas de pago

---
+++ src/Multiservices/PayPayBundle/DataFixtures/ORM/LoadFormasDePagoData.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Arxis (c) 2015 - Todos los derechos reservados.
+ */
+
+/**
+ * Description of LoadFormasDePagoData
+ *
+ * @author Rene Arias <renearias@arxis.la>
+ */
+
+namespace Multiservices\PayPayBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Multiservices\PayPayBundle\Entity\FormasPagos;
+use Multiservices\NotifyBundle\ActionData\ActionParameters;
+
+class LoadFormasDePagoData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+        $formasdepago=$this->loadFormasDePago($manager);
+        foreach($formasdepago as $formadepago)
+        {    
+          $manager->persist($formadepago);
+        }
+        $manager->flush();
+    }
+    
+    private function loadFormasDePago(ObjectManager $manager)
+    {
+        $formasdepago=[];
+        
+        $efectivo=$manager->getRepository('PayPayBundle:FormasPagos')->findByFormaPago('EFECTIVO');
+        if (!$efectivo)
+        {
+          $efectivo = new FormasPagos();
+          $efectivo->setFormaPago("EFECTIVO");
+          $efectivo->setDescripcion("Efectivo al momento de la compra");
+        }
+        
+        
+        $formasdepago[]=$efectivo;
+        
+            
+        return $formasdepago;
+    }
+}

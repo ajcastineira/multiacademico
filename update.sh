5fe91d46e56b344753855d071904c7e4ac95cd76
renearias <rene_811@hotmail.com>
2016-03-26 18:23:31 -0500

    update restored

---
+++ update.sh
@@ -0,0 +1,16 @@
+echo "Actualizando Multiacademico"
+git reset --hard
+git pull --rebase
+composer=$1
+if [ "$composer" == "composer" ]; then
+               echo "Se Actualizara Composer"
+               php composer.phar self-update
+               php composer.phar update
+fi
+
+php bin/console cache:clear --env prod
+php bin/console doctrine:schema:update --dump-sql --force
+php bin/console doctrine:fixtures:load --append
+chmod 755 index.php
+chmod 755 web/app.php
+chmod 755 web/app_dev.php

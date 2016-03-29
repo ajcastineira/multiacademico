f70297987d3f6d25ef291c7bdd1a20a729af50c7
renearias <rene_811@hotmail.com>
2016-03-29 09:20:38 -0500

    dejando el borrado del cache para el final

--- update.sh
+++ update.sh
@@ -7,10 +7,9 @@ if [ "$composer" == "composer" ]; then
                php composer.phar self-update
                php composer.phar update
 fi
-
-php bin/console cache:clear --env prod
 php bin/console doctrine:schema:update --dump-sql --force
 php bin/console doctrine:fixtures:load --append
+php bin/console cache:clear --env prod
 chmod 755 index.php
 chmod 755 web/app.php
 chmod 755 web/app_dev.php

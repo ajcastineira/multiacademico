6fb8e09393562862712638686e89acc3719074d9
renearias <rene_811@hotmail.com>
2016-04-13 14:19:44 -0500

    fix pension

--- src/MultiacademicoBundle/Entity/PensionRepository.php
+++ src/MultiacademicoBundle/Entity/PensionRepository.php
@@ -15,7 +15,7 @@ class PensionRepository extends EntityRepository
             ->createQueryBuilder()->select('p')
                     ->from('MultiacademicoBundle:Pension','p')
                     ->innerJoin('p.factura', 'f')
-                    ->where('f.estado =:estado1')
+                    ->where('f.estado =:estado1 OR f.estado =:estado2')
                     ->setParameter('estado1',  EstadoFacturaType::NOPAGADA)
                     ->setParameter('estado2',   EstadoFacturaType::VENCIDA)
                     ->getQuery()->getResult();

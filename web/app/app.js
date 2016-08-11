d303c41db61e55a7b428af8c5a61f1f52d1b21ce
renearias <rene_811@hotmail.com>
2016-08-11 02:26:37 -0500

    app migrada

--- web/app/app.js
+++ web/app/app.js
@@ -9,128 +9,133 @@
  * Main module of the application.
  */
 
-define([
-    'angular',
-    'angular-couch-potato',
-    'angular-ui-router',
-    'angular-animate',
-    'angular-sanitize',
-    'angular-bootstrap',
+angular.module('app', [
+    'ngSanitize',
+    'ngAnimate',
+    'firebase',
+    'oc.lazyLoad',
+    'restangular',
+    'ui.router',
+    'ui.bootstrap',
     'angular-loading-bar',
-    'angularfire',
-    'oc-lazyload'
-], function (ng, couchPotato) {
+    'blankonConfig',
+    'blankonController',
+    'blankonDirective',
+    // Smartadmin Angular Common Module
+    'SmartAdmin',
+    // App
+    'app.auth',
+    'app.dashboard',
+    'app.users',
+    'app.layout',
+    'app.chat',
+    'app.calendar',
+    'app.inbox',
+    'app.graphs',
+    'app.tables',
+    'app.forms',
+    'app.notificaciones',
+    'app.paypay',
+    'app.paypay.ingresos',
+    'app.ui',
+    'app.widgets',
+    'app.maps',
+    'app.appViews',
+    'app.misc',
+    'app.smartAdmin',
+    //'app.eCommerce',
 
-
-    var app = ng.module('app', [
-        'ngSanitize',
-        'ngAnimate',
-        'firebase',
-        'oc.lazyLoad',
-        'scs.couch-potato',
-        'ui.router',
-        'ui.bootstrap',
-        'angular-loading-bar',
-        'blankonConfig',
-        'blankonController',
-        'blankonDirective',
-        //app
-        'app.auth',
-        'app.dashboard',
-        'app.users',
-        'app.layout',
-        'app.forms',
-        'app.notificaciones',
-        'app.paypay',
-        'app.paypay.ingresos',
-       
      //   'app.paypay.egresos',
-        'multiacademico',
-        'multiacademico.entidad',
-        'multiacademico.estudiantes',
-        'multiacademico.estadisticas',
-        'multiacademico.matriculas',
-        'multiacademico.docentes',
-        'multiacademico.docentes.midistributivo',
-        'multiacademico.proyectosescolares',
-        'multiacademico.aulas',
-        'multiacademico.actividadacademica',
-        'multiacademico.actividadacademicadetalle',
-        'multiacademico.cursos',
-        'multiacademico.distributivos',
-        'multiacademico.malla',
-        'multiacademico.especializaciones',
-        'multiacademico.certificados',
-       'multiacademico.informes',
-        'multiacademico.materias',
-        'multiacademico.pensiones',
-        'multiacademico.representantes',
-   //      'multiacademico.pagos',
-       // 'multiacademico.periodo'
+    'multiacademico',
+    'multiacademico.entidad',
+    'multiacademico.estudiantes',
+    'multiacademico.estadisticas',
+    'multiacademico.matriculas',
+    'multiacademico.docentes',
+    'multiacademico.docentes.midistributivo',
+    'multiacademico.proyectosescolares',
+    'multiacademico.aulas',
+    'multiacademico.actividadacademica',
+    'multiacademico.actividadacademicadetalle',
+    'multiacademico.cursos',
+        
+    //'multiacademico.comportamiento', // no esta completado
+    'multiacademico.distributivos',
+    'multiacademico.malla',
+    'multiacademico.especializaciones',
+    'multiacademico.certificados',
+    'multiacademico.informes',
+    'multiacademico.materias',
+    'multiacademico.pensiones',
+    'multiacademico.representantes',
+    //'multiacademico.pagos',
+    //'multiacademico.periodo'
 
 
 
-    ]);
+])
+.config(function ($provide, $httpProvider, RestangularProvider, $locationProvider) {
 
-   couchPotato.configureApp(app);
-
-    app.config(function ($provide, $httpProvider, $locationProvider) {
-
-        var config = {
+         var config = {
             apiKey: "AIzaSyDPSuC3ADQFoelry5J0O1kjvhJtuCfwK8A",
             authDomain: "multiacademico.firebaseapp.com",
             databaseURL: "https://multiacademico.firebaseio.com",
             storageBucket: "multiacademico.appspot.com",
           };
           firebase.initializeApp(config);
-        
-        $locationProvider.html5Mode(true);
-        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-        // Intercept http calls.
-        $provide.factory('ErrorHttpInterceptor', function ($q) {
-            var errorCounter = 0;
-            function notifyError(rejection){
-                console.log(rejection);
-               /* $.bigBox({
-                    title: rejection.status + ' ' + rejection.statusText,
-                    content: rejection.data,
-                    color: "#C46A69",
-                    icon: "fa fa-warning shake animated",
-                    number: ++errorCounter,
-                    timeout: 6000
-                });*/
-          }
 
-            return {
-                // On request failure
-                requestError: function (rejection) {
-                    // show notification
-                    notifyError(rejection);
+    $locationProvider.html5Mode(true);
+    //$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    // Intercept http calls.
+    $provide.factory('ErrorHttpInterceptor', function ($q) {
+        var errorCounter = 0;
+        function notifyError(rejection){
+            console.log(rejection);
+            $.bigBox({
+                title: rejection.status + ' ' + rejection.statusText,
+                content: rejection.data,
+                color: "#C46A69",
+                icon: "fa fa-warning shake animated",
+                number: ++errorCounter,
+                timeout: 6000
+            });
+        }
 
-                    // Return the promise rejection.
-                    return $q.reject(rejection);
-                },
+        return {
+            // On request failure
+            requestError: function (rejection) {
+                // show notification
+                notifyError(rejection);
 
-                // On response failure
-                responseError: function (rejection) {
-                    // show notification
-                    notifyError(rejection);
-                    // Return the promise rejection.
-                    return $q.reject(rejection);
-                }
-            };
-        });
-        // Add the interceptor to the $httpProvider.
-        $httpProvider.interceptors.push('ErrorHttpInterceptor');
+                // Return the promise rejection.
+                return $q.reject(rejection);
+            },
 
+            // On response failure
+            responseError: function (rejection) {
+                // show notification
+                notifyError(rejection);
+                // Return the promise rejection.
+                return $q.reject(rejection);
+            }
+        };
     });
 
-    app.run(function ($couchPotato, $rootScope, $state, $stateParams) {
-        app.lazy = $couchPotato;
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
-        // editableOptions.theme = 'bs3';
-    });
+    // Add the interceptor to the $httpProvider.
+    $httpProvider.interceptors.push('ErrorHttpInterceptor');
 
-    return app;
+    RestangularProvider.setBaseUrl(location.pathname.replace(/[^\/]+?$/, ''));
+
+})
+.constant('APP_CONFIG', window.appConfig)
+
+.run(function ($rootScope
+    , $state, $stateParams
+    ) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    // editableOptions.theme = 'bs3';
+
 });
+
+

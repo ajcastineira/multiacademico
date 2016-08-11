c3911b7ac71a06989099a4d9ed8229a8a6156922
renearias <rene_811@hotmail.com>
2016-08-10 22:34:10 -0500

    plugins nuevos

---
+++ web/app/calendar/module.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+angular
+.module('app.calendar', ['ngResource','ui.router'])
+.config(function ($stateProvider) {
+
+    $stateProvider
+        .state('app.calendar', {
+            url: '/calendar',
+            views: {
+                content: {
+                    templateUrl: 'app/calendar/views/calendar.tpl.html'
+                }
+            },
+            data:{
+                title: 'Calendar'
+            }
+        });
+});
+
+

e21e6157026fead87d0f807e2282edfc32383f8f
renearias <rene_811@hotmail.com>
2016-08-11 01:47:30 -0500

    layout fix

--- web/app/layout/module.js
+++ web/app/layout/module.js
@@ -9,11 +9,14 @@ angular.module('app.layout', ['ui.router'])
     $stateProvider
         .state('app', {
             abstract: true,
-            views: {
+            template:'<div data-smart-router-animation-wrap="content content@app" data-wrap-for="#content">'+
+                                        '<div data-ui-view="content" data-autoscroll="false"></div>'+
+                                  '</div>',
+            /*views: {
                 root: {
                     templateUrl: Routing.generate('layout',{_format:'html'})
                 }
-            }
+            }*/
         });
     $urlRouterProvider.otherwise('/');
 

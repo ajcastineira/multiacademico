739d62bb7a2c082f835a6e3058a8d939b5f03c58
renearias <rene_811@hotmail.com>
2016-08-11 03:50:02 -0500

    results corrector parche

--- src/AppBundle/Lib/ResultsCorrector.php
+++ src/AppBundle/Lib/ResultsCorrector.php
@@ -12,20 +12,35 @@ use Doctrine\ORM\Query\Expr\Comparison;
 class ResultsCorrector {
     
     public static function validarParte(Comparison $part,$valorBuscado,$valorNuevo){
-            if ($part->getLeftExpr()==$valorBuscado){    
+        
+            if ($part->getLeftExpr()==$valorBuscado){ 
             $newpart=new Comparison($valorNuevo,$part->getOperator(),$part->getRightExpr());
             return $newpart;
             }
             return $part;
         }
+    public static function validarString($part,$valorBuscado,$valorNuevo){
         
+            /*if ($part->getLeftExpr()==$valorBuscado){ 
+            $newpart=new Comparison($valorNuevo,$part->getOperator(),$part->getRightExpr());
+            return $newpart;
+            }*/
+            return $part;
+        }    
     public static function obtenerPartesValidas($partes, $valorBuscado, $valorNuevo)
     {
         if (!($partes instanceof \Doctrine\ORM\Query\Expr\Comparison)){
         $partesvalidas=[];
+        //var_dump($partes, $valorBuscado);    
         foreach ($partes->getParts() as $parte)
             {
-                $partesvalidas[]=self::validarParte($parte, $valorBuscado, $valorNuevo);
+                    if ($parte instanceof Comparison)
+                    {
+                    $partesvalidas[]=self::validarParte($parte, $valorBuscado, $valorNuevo);
+                    }else
+                    {
+                      $partesvalidas[]=self::validarString($parte, $valorBuscado, $valorNuevo);
+                    }    
             }
         $clase=get_class($partes);    
         return new $clase($partesvalidas);

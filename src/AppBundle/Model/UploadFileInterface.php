58db71a6a76ee027de42514f1c8fc14608ad0491
renearias <rene_811@hotmail.com>
2016-10-10 02:03:41 -0500

    interface for entities with files

--- src/AppBundle/Model/UploadFileInterface.php
+++ src/AppBundle/Model/UploadFileInterface.php
@@ -6,9 +6,17 @@ namespace AppBundle\Model;
  *
  * @author Rene Arias <renearias@arxis.la>
  */
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 interface UploadFileInterface {
+    public function getPath();
+    public function setPath($path);
+    public function getFile();
+    public function setFile(UploadedFile $file = null);
     public function getTemp();
     public function setTemp($temp);
     public function getWebPath();
     public function setWebPath($webPath);
+    public function getUploadRootDir();
+    public function getUploadDir();
 }

efa9514ef58e6ae176364919f2ebab73f1eec1b2
renearias <rene_811@hotmail.com>
2016-10-22 15:42:04 -0500

    rutas updated

--- src/MultiacademicoBundle/Controller/MallasController.php
+++ src/MultiacademicoBundle/Controller/MallasController.php
@@ -22,8 +22,10 @@ class MallasController extends Controller
     /**
      * Lists all Materias entities.
      *
-     * @Route("/malla-normal", name="malla-normal")
-     * @Route("/malla-normal/aula/{aula}/{q}/{p}", name="malla-normal-front")
+     * @Route("/malla/normal", name="malla-normal")
+     * @Route("/malla/normal/n/aula/{aula}/{q}/{p}", name="malla-normal-front")
+     * @Route("/malla/estadistica", name="malla-estadistica")
+     * @Route("/malla/estadistica/e/aula/{aula}/{q}/{p}", name="malla-estadistica-front")
      * @Method("GET")
      */
     public function indexAction()
@@ -39,6 +41,26 @@ class MallasController extends Controller
      * @Template()
      */
     public function mallaNormalAction()
+    {
+       $em = $this->getDoctrine()->getManager();
+        $periodo = $em->getRepository('MultiacademicoBundle:Periodos')->find(1);
+        if (!$periodo) {
+            throw $this->createNotFoundException('El periodo no esta configurado.');
+        }
+        $titulo="CUADRO DE CALIFICACIONES FINALES";
+        return array(
+            'titulo'=>$titulo,
+            'periodo'=>$periodo,);
+    }
+    
+    /**
+     * Lists all Materias entities.
+     *
+     * @Route("/api/malla-estadistica", name="malla-estadistica-api", options={"expose":true})
+     * @Method("GET")
+     * @Template()
+     */
+    public function mallaEstadisticaAction()
     {
        $em = $this->getDoctrine()->getManager();
         $periodo = $em->getRepository('MultiacademicoBundle:Periodos')->find(1);

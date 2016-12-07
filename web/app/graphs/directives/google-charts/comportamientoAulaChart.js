e526a0dfe2bd74aa7a73ee7f8c9ce5cf5bb14fef
renearias <rene_811@hotmail.com>
2016-12-07 13:15:11 -0500

    comportamiento de aula grafico

---
+++ web/app/graphs/directives/google-charts/comportamientoAulaChart.js
@@ -0,0 +1,108 @@
+'use strict';
+
+angular.module('app.graphs').directive('comportamientoAulaChart',
+    function (Calificaciones, CALIFICACION_MINIMA, CALIFICACION_META) {
+            return {
+                restrict: 'A',
+                scope: {
+                    titulo: '@',
+                    graphdata: '=',
+                    q: '=',
+                    p: '='
+                },
+                /*compile: function (element, attributes) {
+                   
+                    
+                },*/
+                link: function (scope, element, attributes) {
+                    
+                    google.charts.load('current', {'packages':['corechart', 'controls'],'callback': drawDashboard});
+                    
+                    var $filterDiv = $('<div id="filter_div">filter</div>')
+                        .appendTo(element);
+                    var $chartDiv = $('<div id="chart_div">chart</div>')
+                        .appendTo(element);
+                    
+                    var calificacionMeta=CALIFICACION_META;
+
+                    var listenQ = scope.$watch(attributes.q, function(value) {
+                        google.charts.setOnLoadCallback(drawDashboard);
+                    });
+                    var listenP =scope.$watch(attributes.p, function(value) {
+                        google.charts.setOnLoadCallback(drawDashboard);
+                    });
+                    //{"#3366cc","#dc3912","#ff9900","#109618","#990099","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"}
+                    function drawDashboard() {
+                        
+                        var data = google.visualization.arrayToDataTable(scope.graphdata);
+                        
+                         // Create a dashboard.
+                        var dashboard = new google.visualization.Dashboard(element[0]);
+                        // Create a range slider, passing some options
+                        var rangeSlider = new google.visualization.ControlWrapper({
+                          'controlType': 'CategoryFilter',
+                          'containerId': 'filter_div',
+                          'options': {
+                            'filterColumnLabel': 'Comportamiento',
+                            'ui':{
+                                caption:'Calificacion',
+                            }
+                          }
+                        });
+                        
+                        var options = {
+                          title: scope.titulo,
+                          height: 300,
+                          //'width': 300,
+                          colors: ["#3366cc","#109618","#990099","#ff9900","#dc3912","#0099c6","#dd4477","#66aa00","#b82e2e","#316395","#994499","#22aa99","#aaaa11","#6633cc","#e67300","#8b0707","#651067","#329262","#5574a6","#3b3eac","#b77322","#16d620","#b91383","#f4359e","#9c5935","#a9c413","#2a778d","#668d1c","#bea413","#0c5922","#743411"],
+                          vAxis: { title: 'Cantidad Alumnos',
+                            viewWindow: {
+                                max: 10
+                            },
+                           maxValue:10},
+                          hAxis: {title: 'Comportamiento',
+                                  showTextEvery: 1,
+                                  maxTextLines: 5,
+                                  textStyle: {
+                                     fontSize: 8
+                                 }
+                                },
+                          //isStacked: 'percent',      
+                          /*seriesType: 'bars',
+
+                          series: {
+                                   1: {type: 'line'},
+                                   2: {type: 'line'}
+                                    },*/
+                          //curveType: 'function',
+                          //legend: { position: 'bottom' },
+                          //pointSize: 5
+                        };
+                        
+                        // Create a combo chart, passing some options
+                        var chart = new google.visualization.ChartWrapper({
+                          'chartType': 'PieChart',
+                          'containerId': 'chart_div',
+                          'options': options
+                        });
+                                                
+                        dashboard.bind(rangeSlider, chart);
+
+                        // Draw the dashboard.
+                        dashboard.draw(data);
+                        
+                      }
+
+                    scope.$on('destroy', function(){
+                        listenQ();
+                        listenP();
+                    });
+                }
+            };
+});
+
+
+     
+
+      
+        
